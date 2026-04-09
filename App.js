b57382b432cc32887201438f0d5ba3b6f516ab97
Dhea <242310045@student.ibik.ac.id>
2026-04-09 10:00:33 +0700

    Pertemuan-2

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LandingPage from './components/latihan-2'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <LandingPage />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e6b8b8',
     alignItems: 'center',
     justifyContent: 'center',
   },
